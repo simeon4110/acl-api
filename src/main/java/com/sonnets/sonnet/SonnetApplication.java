4a4ff641649a207c37b9fa2c25bea008854cca5c
Josh Harkema <josh@joshharkema.com>
2018-05-15 10:59:50 -0600

    Tweaks to allow text file ingest.

--- src/main/java/com/sonnets/sonnet/SonnetApplication.java
+++ src/main/java/com/sonnets/sonnet/SonnetApplication.java
@@ -1,14 +1,21 @@
 package com.sonnets.sonnet;
 
+import com.sonnets.sonnet.models.Sonnet;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * @author Josh Harkema
  */
 @SpringBootApplication
-@EnableJpaAuditing
+@ComponentScan(basePackages = {"com.sonnets.sonnet"})
+@EnableAutoConfiguration
+@EntityScan(basePackageClasses = Sonnet.class)
+@EnableJpaRepositories("com.sonnets.sonnet.repositories")
 public class SonnetApplication {
 
     public static void main(String[] args) {
