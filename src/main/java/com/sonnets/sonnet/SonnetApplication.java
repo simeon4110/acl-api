fe7a027405c44f03fd19a96365ed4f1b5a643d59
Josh Harkema <josh@joshharkema.com>
2018-05-27 15:48:10 -0600

    Maybe it will work now...

--- src/main/java/com/sonnets/sonnet/SonnetApplication.java
+++ src/main/java/com/sonnets/sonnet/SonnetApplication.java
@@ -1,23 +1,14 @@
 package com.sonnets.sonnet;
 
-import com.sonnets.sonnet.persistence.models.Sonnet;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * @author Josh Harkema
  */
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.sonnets.sonnet"})
-@EnableAutoConfiguration
-@EntityScan(basePackageClasses = Sonnet.class)
-@EnableJpaRepositories("com.sonnets.sonnet.persistence.repositories")
 public class SonnetApplication extends SpringBootServletInitializer {
 
     @Override
