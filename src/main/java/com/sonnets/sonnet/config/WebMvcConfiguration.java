9645219defeaad94f0b627dfe6dc42c777eb1c13
Josh Harkema <josh@joshharkema.com>
2018-05-27 13:19:53 -0600

    Maybe it will work now...

--- src/main/java/com/sonnets/sonnet/config/WebMvcConfiguration.java
+++ src/main/java/com/sonnets/sonnet/config/WebMvcConfiguration.java
@@ -22,6 +22,7 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/sonnet/login").setViewName("login_processing");
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
     }
 
