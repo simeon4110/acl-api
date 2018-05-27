dba020aac09be67533a15b5147c77a3277928d96
Josh Harkema <josh@joshharkema.com>
2018-05-27 13:47:18 -0600

    Maybe it will work now...

--- src/main/java/com/sonnets/sonnet/config/SecurityConfig.java
+++ src/main/java/com/sonnets/sonnet/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(final HttpSecurity http) throws Exception {
         // @formatter:off
         http.authorizeRequests()
-            .antMatchers("/sonnet/login", "/sonnet/login/**").permitAll()
+            .antMatchers("/sonnet/login", "/sonnet/login/**", "/login", "/login/**").permitAll()
             .antMatchers("/css/**").permitAll()
             .antMatchers("/").permitAll()
             .antMatchers("/lookup/", "/lookup/**").permitAll()
@@ -55,7 +55,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .antMatchers("/admin", "/admin/add/", "/admin/**").hasAuthority("ADMIN")
             .antMatchers("/profile", "/profile/**").hasAuthority("USER")
             .anyRequest().authenticated()
-            .and().formLogin().loginPage("/sonnet/login").permitAll()
+            .and().formLogin().loginPage("/login").permitAll()
             .and().logout().permitAll()
             .and().csrf();
             // @formatter:on
