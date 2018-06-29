b226352488523d67d2743e7ba798bd617323963b
simeon4110 <josh@joshharkema.com>
2018-06-28 20:09:29 -0600

    Gutted unneeded services from both Browse and Lookup Controllers. So much unneeded code.

--- src/main/java/com/sonnets/sonnet/controllers/BrowseController.java
+++ src/main/java/com/sonnets/sonnet/controllers/BrowseController.java
@@ -1,17 +1,10 @@
 package com.sonnets.sonnet.controllers;
 
-import com.sonnets.sonnet.persistence.models.Sonnet;
-import com.sonnets.sonnet.services.SonnetDetailsService;
-import com.sonnets.sonnet.tools.Pager;
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.Objects;
 
 /**
  * Handles requests to the browse page.
@@ -21,54 +14,18 @@ import java.util.Objects;
 @Controller
 public class BrowseController {
     private static final Logger LOGGER = Logger.getLogger(BrowseController.class);
-
     private static final String PAGE_TITLE_CONST = "pageTitle";
     private static final String PAGE_TITLE_VALUE = "Browse";
-    private static final int[] PAGE_SIZES = {5, 10, 20, 50};
-    private static final int BUTTONS_TO_SHOW = 5;
-    private static final String DEFAULT_SORT = "lastName";
-    private static final String[][] SORT_BY = {{"firstName", "First Name"}, {DEFAULT_SORT, "Last Name"},
-            {"title", "Title"}, {"publicationYear", "Publication Year"}};
-
-
-    private final SonnetDetailsService sonnetDetailsService;
-
-
-    @Autowired
-    public BrowseController(SonnetDetailsService sonnetDetailsService) {
-        this.sonnetDetailsService = sonnetDetailsService;
-    }
 
     /**
      * @param pageRequest the page request passed.
      * @return a paged HTML page of all the sonnets paged. Default size 20 items.
      */
-    @SuppressWarnings("SameReturnValue")
     @GetMapping(value = "/browse")
     public String getPagedList(Pageable pageRequest, Model model) {
-        int pageSize = pageRequest.getPageSize();
-        String[] sort = pageRequest.getSort().toString().split(":");
-        Page<Sonnet> sonnets = sonnetDetailsService.getAllSonnetsPaged(pageRequest);
-
-        // Catch unsorted requests and set the default.
-        String sortBy = sort[0].replace(':', ' ').trim();
-        if (Objects.equals(sortBy, "UNSORTED")) {
-            sortBy = DEFAULT_SORT;
-        }
-
-        // One is subtracted to fix strange offset issues.
-        Pager pager = new Pager(sonnets.getTotalPages() - 1, pageRequest.getPageNumber(), BUTTONS_TO_SHOW);
-
-        LOGGER.debug("Page size: " + pageSize);
-        LOGGER.debug("Sort by: " + sortBy);
-
-        model.addAttribute("page", sonnets);
-        model.addAttribute("pageSizes", PAGE_SIZES);
-        model.addAttribute("selectedPageSize", pageSize);
-        model.addAttribute("pager", pager);
-        model.addAttribute("selectedSortBy", sortBy);
-        model.addAttribute("sortBy", SORT_BY);
+        LOGGER.fatal("Showing browse page.");
         model.addAttribute(PAGE_TITLE_CONST, PAGE_TITLE_VALUE);
+
         return "browse";
     }
 
