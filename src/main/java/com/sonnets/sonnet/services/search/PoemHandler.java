51231a5dcc12d8c5d2dec50fee25ad33c60acdd9
simeon4110 <josh@joshharkema.com>
2018-10-17 14:40:11 -0600

    #55 Test / Repair / Build out the Confirmation Module - pulled out more unneeded code.

--- src/main/java/com/sonnets/sonnet/services/search/PoemHandler.java
+++ src/main/java/com/sonnets/sonnet/services/search/PoemHandler.java
@@ -16,19 +16,7 @@ public interface PoemHandler {
 
     private static Query processQuery(SearchDto dto) {
         BooleanQuery.Builder query = new BooleanQuery.Builder();
-        if (dto.getAuthor() != null) {
-            if (dto.getAuthor().getFirstName() != null && !dto.getAuthor().getFirstName().isEmpty()) {
-                query.add(new TermQuery(new Term(SearchConstants.AUTHOR_FIRST_NAME,
-                                dto.getAuthor().getFirstName().toLowerCase())),
-                        BooleanClause.Occur.MUST);
-            }
-            if (dto.getAuthor().getLastName() != null && !dto.getAuthor().getLastName().isEmpty()) {
-                query.add(new TermQuery(new Term(SearchConstants.AUTHOR_LAST_NAME,
-                                dto.getAuthor().getLastName().toLowerCase())),
-                        BooleanClause.Occur.MUST);
-            }
-        }
-        if (dto.getTitle() != null && !dto.getTitle().isEmpty()) {
+        if (!dto.getTitle().isEmpty()) {
             query.add(new FuzzyQuery(new Term(SearchConstants.TITLE, dto.getTitle().toLowerCase()),
                             SearchConstants.EDIT_DISTANCE, SearchConstants.PREFIX_LENGTH),
                     BooleanClause.Occur.MUST);
@@ -40,14 +28,14 @@ public interface PoemHandler {
                     dto.getPublicationYear() + SearchConstants.INT_DISTANCE, true, true
             ), BooleanClause.Occur.MUST);
         }
-        if (dto.getPeriod() != null && !dto.getPeriod().isEmpty()) {
+        if (!dto.getPeriod().isEmpty()) {
             query.add(new TermQuery(new Term(SearchConstants.PERIOD, dto.getPeriod().toLowerCase())),
                     BooleanClause.Occur.MUST);
         }
-        if (dto.getText() != null && !dto.getText().isEmpty()) {
+        if (!dto.getText().isEmpty()) {
             query.add(ParseText.getPhraseQuery(dto), BooleanClause.Occur.MUST);
         }
-        if (dto.getForm() != null && !dto.getForm().isEmpty()) {
+        if (!dto.getForm().isEmpty()) {
             query.add(new TermQuery(new Term(SearchConstants.POEM_FORM, dto.getForm())), BooleanClause.Occur.MUST);
         }
         if (dto.getAuthor() != null) {
