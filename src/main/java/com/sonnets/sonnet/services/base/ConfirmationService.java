038ef6fe35abf4825ffba753cb19e7e614c7b967
simeon4110 <josh@joshharkema.com>
2019-03-24 18:37:57 -0600

    #85 Fix Search - added subject to testing messages.
    Signed-off-by: simeon4110 <josh@joshharkema.com>

--- src/main/java/com/sonnets/sonnet/services/base/ConfirmationService.java
+++ src/main/java/com/sonnets/sonnet/services/base/ConfirmationService.java
@@ -109,6 +109,7 @@ public class ConfirmationService {
         if (poem.isTesting()) { // Catch incorrect testing confirmations.
             MessageDto messageDto = new MessageDto();
             messageDto.setUserTo("jharkema");
+            messageDto.setSubject("INCORRECT - testing sonnet");
             messageDto.setUserFrom(principal.getName());
             messageDto.setContent(principal.getName() + " has NOT identified a testing sonnet correctly: "
                     + poem.getTitle());
@@ -151,6 +152,7 @@ public class ConfirmationService {
 
             MessageDto messageDto = new MessageDto();
             messageDto.setUserTo("jharkema");
+            messageDto.setSubject("CORRECT - testing sonnet");
             messageDto.setUserFrom(principal.getName());
             messageDto.setContent(principal.getName() + " has correctly identified a testing sonnet: "
                     + poem.getTitle() + "\nWith message: " + dto.getMessage());
